5d19ee1e8c876633c14e0fd86ffef17268b33c77
Ray Cannon <w7glf@arrl.net>
2025-03-22 11:55:26 -0700

    Update ps2_keyboard__keyer_w7glf.ino

--- ps2_keyboard__keyer_w7glf.ino
+++ ps2_keyboard__keyer_w7glf.ino
@@ -1,13 +1,5 @@
 #include <Keyboard.h>
 #include <KeyboardLayout.h>
-#include <Keyboard_da_DK.h>
-#include <Keyboard_de_DE.h>
-#include <Keyboard_es_ES.h>
-#include <Keyboard_fr_FR.h>
-#include <Keyboard_hu_HU.h>
-#include <Keyboard_it_IT.h>
-#include <Keyboard_pt_PT.h>
-#include <Keyboard_sv_SE.h>
 
 #include <PS2Keyboard.h>
  
